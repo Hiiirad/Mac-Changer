aea7bb7a77e79b53343c18957288137bfe5f4bc6
Hirad Rasoolinejad <hirad.rad@gmail.com>
2019-01-24 19:30:59 +0330

    added regex function to check mac address

--- mac-changer.py
+++ mac-changer.py
@@ -1,7 +1,13 @@
 from subprocess import call
+from re import search
 
 mac = input("Please insert your new mac: ")
-# 00:11:22:33:44:55
+if search(string=mac, pattern=r"^([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}$"):
+    print("Valid mac")
+else:
+    print("Invalid mac. Check it and try again")
+    quit()
+
 call("ifconfig eth0 down", shell=True)
 call("ifconfig eth0 hw ether {0}".format(mac), shell=True)
 call("ifconfig eth0 up", shell=True)
