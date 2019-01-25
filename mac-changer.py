74c79d0410619029c90dac36627700e8339ceef7
Hirad Rasoolinejad <hirad.rad@gmail.com>
2019-01-25 23:06:06 +0330

    Update mac-changer.py
    Adding comment to code

--- mac-changer.py
+++ mac-changer.py
@@ -1,6 +1,7 @@
 from subprocess import call
 from re import search
 
+# Validating mac address
 mac = input("Please insert your new mac: ")
 if search(string=mac, pattern=r"^([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}$"):
     print("Valid mac")
@@ -8,6 +9,7 @@ else:
     print("Invalid mac. Check it and try again")
     quit()
 
+# Start changing mac address
 call("ifconfig eth0 down", shell=True)
 call("ifconfig eth0 hw ether {0}".format(mac), shell=True)
 call("ifconfig eth0 up", shell=True)
